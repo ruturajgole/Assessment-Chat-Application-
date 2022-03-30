b5ee69defb10621c52cc894ed6a40ca99bd3da50
Ruturaj Gole <ruturajpokemon1@gmail.com>
2022-03-30 15:48:52 -0600

    Fix order of messages

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -8,7 +8,7 @@ const Messages = (props) => {
 
   return (
     <Box>
-      {messages.map((message) => {
+      {messages.sort((a, b) => a.id - b.id).map((message) => {
         const time = moment(message.createdAt).format('h:mm');
 
         return message.senderId === userId ? (
