ae0e9346e64cb41d2b7eda14745d9ff94a60b845
Ruturaj Gole <ruturajpokemon1@gmail.com>
2022-05-15 22:35:50 -0600

    Add Table Group

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,17 +1,24 @@
 const Conversation = require("./conversation");
 const User = require("./user");
 const Message = require("./message");
+const Group = require("./group");
 
 // associations
 
-User.hasMany(Conversation);
-Conversation.belongsTo(User, { as: "user1" });
-Conversation.belongsTo(User, { as: "user2" });
+//User.hasMany(Conversation);
+//Conversation.belongsTo(User, { as: "user1" });
+//Conversation.belongsTo(User, { as: "user2" });
 Message.belongsTo(Conversation);
 Conversation.hasMany(Message);
+
+User.hasMany(Group);
+Conversation.hasMany(Group);
+Group.belongsTo(User, { as: "user" });
+Group.belongsTo(Conversation, { as: "conversation" });
 
 module.exports = {
   User,
   Conversation,
-  Message
+  Message,
+  Group
 };
